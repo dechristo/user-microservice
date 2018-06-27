60521f698cc026b869b322ad3a1dbc15eea6d853
Luiz Eduardo <dudu.christo@gmail.com>
2018-06-27 11:12:41 +0200

    Update requirements and tests db connection

--- router.py
+++ router.py
@@ -1,6 +1,9 @@
+import _mysql
 from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    db=_mysql.connect("localhost","root","verysecure","userms")
     return jsonify({'msg':'user microservice running...'})
