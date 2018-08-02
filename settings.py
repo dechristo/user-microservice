179f998657c59fc801c2cadb1de98523eb65d2a8
Luiz Eduardo <dudu.christo@gmail.com>
2018-08-02 12:02:20 +0200

    Add crytography key path

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 import os
-import sys
 from src.utils.json_reader import JsonReader
 
 class Settings:
 
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    KEY = JsonReader.read(PROJECT_ROOT + "/.key.json").get("key")
     ADDRESS_API = "https://api.postmon.com.br/v1/cep/{0}"
-    db_settings = JsonReader.read(PROJECT_ROOT + '/db.json')
+    db_settings = JsonReader.read(PROJECT_ROOT + "/db.json")
     DB_SETTINGS = {
         "hostname": db_settings.get("hostname"),
         "db": db_settings.get("db"),
